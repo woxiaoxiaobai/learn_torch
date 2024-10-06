49558e5826f9c96b86f6b2d0d756901e98e7ef99
ZhangYixin <60743508+woxiaoxiaobai@users.noreply.github.com>
2024-10-06 16:23:09 +0800

    添加cnn模型代码

---
+++ models/cnn.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+# 定义CNN模型
+class CNN(nn.Module):  
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 16, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.out = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output
